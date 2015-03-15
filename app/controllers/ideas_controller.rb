aff8816436f1dfd50819fca1a3084e600cb119c7
Pei Cheng <duffy.cheng@gmail.com>
2015-03-15 16:35:26 +0800

    initial commit

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,80 @@
+class IdeasController < ApplicationController
+  #進入[:show, :edit, :update, :destroy] 之前 run set_idea function first, at the bottom
+  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+
+  # GET /ideas
+  # GET /ideas.json
+  def index
+    @ideas = Idea.all
+  end
+
+  # GET /ideas/1
+  # GET /ideas/1.json
+  def show
+  end
+
+  # GET /ideas/new
+  def new
+    @idea = Idea.new
+  end
+
+  # GET /ideas/1/edit
+  def edit
+  end
+
+  # POST /ideas
+  # POST /ideas.json
+  def create
+    @idea = Idea.new(idea_params)
+
+    respond_to do |format|
+      if @idea.save
+        format.html { redirect_to @idea, notice: 'Idea was successfully created.' }
+        format.json { render :show, status: :created, location: @idea }
+      else
+        format.html { render :new }
+        format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /ideas/1
+  # PATCH/PUT /ideas/1.json
+  def update
+    #only handle html format, no JSON for JS MVC, idea_params may have validation true/false
+    # respond_to do |format|
+      if @idea.update(idea_params)
+        redirect_to @idea, notice: 'Idea was successfully updated.'
+        # format.html { redirect_to @idea, notice: 'Idea was successfully updated.' }
+        #format.json { render :show, status: :ok, location: @idea }
+      else
+        render :edit
+        # format.html { render :edit }
+        #format.json { render json: @idea.errors, status: :unprocessable_entity }
+      end
+    # end
+  end
+
+  # DELETE /ideas/1
+  # DELETE /ideas/1.json
+  def destroy
+    @idea.destroy
+    respond_to do |format|
+      format.html { redirect_to ideas_url, notice: 'Idea was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_idea
+      @idea = Idea.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    # for create, update...function, process first and then feed into the function 
+    # idea 的 key 只允許 name, description, picture 
+    def idea_params
+      params.require(:idea).permit(:name, :description, :picture)
+    end
+end
