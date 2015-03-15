1b0bac2a159f996e0c052c124ad1ab6be8056f47
Pei Cheng <duffy.cheng@gmail.com>
2015-03-15 17:09:24 +0800

    commit

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -11,6 +11,8 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
+    @comments = @idea.comments.all
+    @comment = @idea.comments.build
   end
 
   # GET /ideas/new
